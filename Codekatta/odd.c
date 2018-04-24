9fa09e88c567fc143c72bd5ee01484ea305cb778
Rajakokila <38651092+Rajakokila@users.noreply.github.com>
2018-04-24 14:39:53 +0530

    Create odd.c

---
+++ Codekatta/odd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+  int a;
+  printf("Enter the number..");
+  scanf("%d",&a);
+  if(a%2==0)
+  {
+     printf("Even");
+    
+  }
+  else
+  {
+    printf("odd");
+    return 0;
+  }
+}
