cbd4c8b74979cbb0b2bbe6de24204e8197ba99ca
Rajakokila <38651092+Rajakokila@users.noreply.github.com>
2018-04-24 14:31:44 +0530

    Create positive.c

---
+++ Codekatta/positive.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(void)
+{
+ int a,b;
+ printf("Enter the number");
+ scanf("%d%d",&a,&b);
+   {
+     if(a>0)
+     {
+       printf("positive");
+     }
+     elseif(a<0)
+     {
+       printf("Negative");
+     }
+     else
+     {
+       printf("Zero");
+     } 
+   }
+ return 0;
+}
